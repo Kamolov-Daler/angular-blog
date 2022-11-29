5f4b65d7ab21346d1e053c8aaf09243ba0b04e02
Kamolov-Daler <daler.kamolov.19@mail.ru>
2022-11-30 02:56:38 +0500

    create post form with Text Editor

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/shared/interfaces';
 import { AuthService } from '../shared/services/auth.service';
 
@@ -13,15 +13,23 @@ export class LoginPageComponent implements OnInit {
 
   form: any = '';
   submited: boolean = false;
+  message: string = ''
 
   constructor(
-    private auth: AuthService,
-    private router: Router
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
   ) {
 
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params['loginAgain']) {
+        this.message = 'Пожалуйста введите данные'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
@@ -43,8 +51,10 @@ export class LoginPageComponent implements OnInit {
 
     this.auth.login(user).subscribe(() => {
       this.form.reset()
-      this.submited = false
       this.router.navigate(['/admin', 'dashboard'])
+      this.submited = false
+    }, () => {
+      this.submited = false
     })
   }
 
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpErrorResponse} from '@angular/common/http'
 import { FbAuthResponse, User } from "src/app/shared/interfaces";
-import { Observable, tap } from "rxjs";
+import { catchError, Observable, Subject, tap, throwError } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable() 
 
 export class AuthService {
+
+    public error$: Subject<string> = new Subject<string>()
+
     constructor(private http: HttpClient) {
 
     }
@@ -24,8 +27,10 @@ export class AuthService {
 
     login(user: User): Observable<any> {
        
-        return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user).pipe(
-            tap(this.setToken)
+        return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+        .pipe(
+            tap(this.setToken),
+            catchError(this.handleError.bind(this))
         )
     }
 
@@ -35,6 +40,24 @@ export class AuthService {
 
     isAuthehticated(): boolean {
         return !!this.token
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        const {message} = error.error.error 
+
+        switch (message) {
+            case 'INVALID_EMAIL':
+                this.error$.next('Неверный email')
+                break
+                case 'INVALID_PASSWORD':
+                this.error$.next('Неверный пароль')
+                break
+                case 'EMAIL_NOT_FOUND':
+                this.error$.next('Такого email не существует')
+                break
+        }
+
+        return throwError(message)
     }
 
     private setToken(response: any) {
